43d01d850b080c780658facf14b6a7b15c26d19c
Flor Martinez <floormartinez.fmm@gmail.com>
2021-04-08 08:50:02 +0200

    first auth form template created

--- Bookiernes/urls.py
+++ Bookiernes/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.contrib.auth.views import LoginView
 from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('staff/', LoginView.as_view(template_name='auth/staff_login.html'), name="login")
 ]
