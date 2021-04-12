7db955f329c2a6c088c0aed36f30c7175cd65938
aaron-at97 <arenas.aaron97@gmail.com>
2021-04-12 14:17:22 +0200

    TEMPLATES + CSS

--- Bookiernes/urls.py
+++ Bookiernes/urls.py
@@ -13,13 +13,9 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from msilib.schema import ListView
-from time import timezone
-
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic import ListView
-from eBook.models import Staff
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
