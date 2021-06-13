2b3f94369c2e50a6bc8aeeb1cf329b8ed262d96b
aaron-at97 <arenas.aaron97@gmail.com>
2021-06-13 23:03:11 +0200

    urls arreglo imagenes libros, etc

--- eBook/views.py
+++ eBook/views.py
@@ -1,10 +1,19 @@
+import json
+import sys
+import os
+
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.http import HttpResponseRedirect
+
+from urllib import request, parse
+
+from django.shortcuts import render
 from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView
 
-from eBook.models import Editor, EditorChief, Writer, ChiefDesigner, Designer, CTO, Developer, Notification, Book
+from Bookiernes.settings import BASE_DIR
+from eBook.models import Editor, EditorChief, Writer, ChiefDesigner, Designer, CTO, Developer, Clients, Notification, \
+    Book
 
 
 class LoginRequiredMixinStaff(object):
@@ -28,7 +37,8 @@ def get_member(user):
         return CTO.objects.get(user=user)
     elif Developer.objects.filter(user=user).exists():
         return Developer.objects.get(user=user)
-
+    elif Clients.objects.filter(user=user).exists():
+        return Clients.objects.get(user=user)
     return None
 
 
@@ -56,6 +66,7 @@ class editorsChief(TemplateView):
 
         return context
 
+
 class notifyEs(TemplateView):
     model = Notification
     template_name = 'books/escritor/notificaciones.html'
@@ -66,6 +77,7 @@ class notifyEs(TemplateView):
         context['notify'] = Notification.objects.filter()
 
         return context
+
 
 class notifyEd(TemplateView):
     model = Notification
@@ -79,6 +91,7 @@ class notifyEd(TemplateView):
 
         return context
 
+
 class notifyDis(TemplateView):
     model = Notification
     template_name = 'books/diseñador/notificaciones.html'
@@ -89,6 +102,7 @@ class notifyDis(TemplateView):
         context['notify'] = Notification.objects.filter()
 
         return context
+
 
 class notifyMaq(TemplateView):
     model = Notification
@@ -101,6 +115,7 @@ class notifyMaq(TemplateView):
 
         return context
 
+
 class disenyChief(TemplateView):
     second_model = Book
     template_name = 'books/diseñador/disenyo_asig.html'
@@ -111,3 +126,154 @@ class disenyChief(TemplateView):
         context['role'] = member.role
 
         return context
+
+
+class LeerLibro(TemplateView):
+    template_name = 'books/client/leer_libro.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(LeerLibro, self).get_context_data(**kwargs)
+        data, pagina, libro = self.read_file(self.kwargs['libropk'], self.kwargs['pagnum'])
+
+        context['data'] = data
+        context['anterior'] = int(pagina) - 1
+        context['pagina'] = pagina
+        context['siguiente'] = int(pagina) + 1
+        context['libro'] = libro
+
+        return context
+
+    def read_file(self, libro, pagina):
+        data = ""
+        try:
+            with open(os.path.join(BASE_DIR, 'static') + "/img/Libros/" + libro + "/" + pagina + ".txt", "r",
+                      encoding='utf8') as file:
+                # Strip lines
+                data = file.read()
+        except Exception as e:
+            with open(os.path.join(BASE_DIR, 'static') + "/img/Libros/" + libro + "/" + pagina + ".txt", "r",
+                      encoding='iso-8859-1') as file:
+                # Strip lines
+                data = file.read()
+        return data, pagina, libro
+
+
+def traducir(request):
+    libro = request.GET.get("libro")
+    idioma = request.GET.get("idioma")
+
+    return render(request, 'books/editor/solicitud_traduccion2.html', {"libropk": libro, "pagnum": idioma})
+
+class TraducirLibro(TemplateView):
+    template_name = 'books/editor/solicitud_traduccion2.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(TraducirLibro, self).get_context_data(**kwargs)
+        data = self.read_file(self.kwargs['libropk'], self.kwargs['pagnum'])
+        context['data'] = data
+
+        return context
+
+    def read_file(self, libro, idioma):
+        data = ""
+        try:
+            with open(os.path.join(BASE_DIR, 'static') + "/img/Libros/" + libro + "/" + "1.txt", "r",
+                      encoding='utf8') as file:
+                # Strip lines
+                data = file.read()
+                data = self.translate(data, "es", idioma, "https://translate.astian.org/translate", libro)
+        except Exception as e:
+            with open(os.path.join(BASE_DIR, 'static') + "/img/Libros/" + libro + "/" + "1.txt", "r",
+                      encoding='iso-8859-1') as file:
+                # Strip lines
+                data = file.read()
+                data = self.translate(data, "es", idioma, "https://translate.astian.org/translate", libro)
+
+        return data
+
+    def translate(self, q, source, idioma, url, libro):
+        """Connect to LibreTranslate API
+        Args:
+            q (str): The text to translate
+            source (str): The source language code (ISO 639)
+            target (str): The target language code (ISO 639)
+
+        Returns: The translated text
+        """
+        params = {"q": q, "source": source, "target": idioma}
+
+        url_params = parse.urlencode(params)
+
+        req = request.Request(url, data=url_params.encode())
+
+        try:
+            response = request.urlopen(req)
+        except Exception as e:
+            print(e, sys.stderr)
+            return None
+
+        try:
+            response_str = response.read().decode()
+        except Exception as e:
+            print(e, sys.stderr)
+            return None
+
+        data = ""
+        data = str(json.loads(response_str))
+        nueva = ""
+        ant = ""
+        data = data[20:len(data)-2]
+
+        for c in data:
+            if c == '\\':
+                nueva = nueva + " \n "
+            elif ant == '\\':
+                pass
+            else:
+                nueva = nueva + c
+            ant = c
+        try:
+            with open(os.path.join(BASE_DIR, 'static') + "/img/Libros/" + libro + "/" + idioma + "/1.txt", 'w',
+                      encoding='utf8') as file_write:
+                # write json data into file
+                file_write.write(nueva)
+                file_write.close()
+
+        except Exception as e:
+            with open(os.path.join(BASE_DIR, 'static') + "/img/Libros/" + libro + "/" + idioma + "/1.txt", 'w',
+                      encoding='iso-8859-1') as file_write:
+                # Strip lines
+                # write json data into file
+                file_write.write(nueva)
+                file_write.close()
+
+        return data
+
+
+
+class Verdemo(TemplateView):
+    template_name = 'home/ver_demo.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(Verdemo, self).get_context_data(**kwargs)
+        data, libro = self.read_file(self.kwargs['libropk'], "1")
+
+        context['data'] = data
+        context['libro'] = libro
+        context['autor'] = self.kwargs['autor']
+
+        return context
+
+    def read_file(self, libro, pagina):
+        data = ""
+        try:
+            with open(os.path.join(BASE_DIR, 'static') + "/img/Libros/" + libro + "/" + pagina + ".txt", "r",
+                      encoding='utf8') as file:
+                # Strip lines
+                data = file.read()
+        except Exception as e:
+            with open(os.path.join(BASE_DIR, 'static') + "/img/Libros/" + libro + "/" + pagina + ".txt", "r",
+                      encoding='iso-8859-1') as file:
+                # Strip lines
+                data = file.read()
+        return data, libro
